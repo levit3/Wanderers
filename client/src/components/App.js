98454097cc93297a31311c60ec44968ebfeedfd3
14justin <justinmwangi40@gmail.com>
2024-07-18 18:47:51 +0300

    Wanderers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/AuthForms/Login';
+import Register from './components/AuthForms/Register';
+import UserDetails from './components/User/UserDetails';
+import UserList from './components/User/UserList';
+import UserReview from './components/User/UserReview';
+import UserSignup from './components/User/UserSignup';
+import Header from './components/Header';
+import ReviewForm from './components/ReviewForm';
+import TravelGuideDetail from './components/TravelGuideDetail';
+import TravelGuideList from './components/TravelGuideList';
+import './App.css';
 
-function App() {
-  return <h1>Project Client</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={TravelGuideList} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/user/:id" component={UserDetails} />
+          <Route path="/users" component={UserList} />
+          <Route path="/review/:travelGuideId" component={UserReview} />
+          <Route path="/user-signup" component={UserSignup} />
+          <Route path="/review-form" component={ReviewForm} />
+          <Route path="/travel-guide/:id" component={TravelGuideDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
